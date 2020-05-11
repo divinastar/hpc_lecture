d70ba0450639f8b8d75593efe6eb0a03d46c85ef
19M38372 <peemapot57@gmail.com>
2020-05-11 18:52:25 +0900

    Homework1 19M38372

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -6,24 +6,40 @@ int main() {
   int n = 50;
   int range = 5;
   std::vector<int> key(n);
+
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
-    printf("%d ",key[i]);
-  }
+    printf("%d ",key[i]); 
+
+}
   printf("\n");
 
-  std::vector<int> bucket(range); 
+  std::vector<int> bucket(range);
+#pragma omp parallel for
   for (int i=0; i<range; i++) {
     bucket[i] = 0;
   }
+#pragma omp parallel for shared(bucket)
   for (int i=0; i<n; i++) {
+#pragma omp atomic update
     bucket[key[i]]++;
-  }
-  for (int i=0, j=0; i<range; i++) {
-    for (; bucket[i]>0; bucket[i]--) {
+ }
+
+
+#pragma omp parallel for 
+  for (int i=0; i<range; i++) {
+    int j = 0;
+#pragma omp parallel for reduction(+:j)
+  for(int k=0; k<i; k++){
+     j += bucket[k];
+    }
+
+#pragma omp parallel for
+  for (int a=bucket[i]; a>0; a--) {    
       key[j++] = i;
     }
   }
+
 
   for (int i=0; i<n; i++) {
     printf("%d ",key[i]);
