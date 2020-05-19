9a24ad2ae2589e71610777fcc9621a780a3a43be
divinastar <56137998+divinastar@users.noreply.github.com>
2020-05-20 08:06:45 +0900

    Update 13_nbody.cpp

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -21,28 +21,30 @@ int main() {
   __m256 xtemp = _mm256_load_ps(x);
   __m256 ytemp = _mm256_load_ps(y);
   
+//Permute constant
   int idx[N] = {1,2,3,4,5,6,7,0};
   __m256i ivec = _mm256_load_si256((__m256i*)idx);
-
-  float show1[N], show2[N];
-
+	
 
   for(int i=0; i<N-1; i++) {
+//Shifting xtemp and ytemp
       xtemp = _mm256_permutevar8x32_ps(xtemp,ivec);
       ytemp = _mm256_permutevar8x32_ps(ytemp,ivec);    
 
+//Calculate rx, ry by subtracting xvec with xtemp
       __m256 rx = _mm256_sub_ps(xvec,xtemp);
       __m256 ry = _mm256_sub_ps(yvec,ytemp);
 
+//Calculate rx2, ry2, rx2_ry2, and so on...
       __m256 rx2 = _mm256_mul_ps(rx,rx);
       __m256 ry2 = _mm256_mul_ps(ry,ry);
       __m256 rx2_ry2 = _mm256_add_ps(rx2,ry2);
-      __m256 r = _mm256_sqrt_ps(rx2_ry2);
-//      __m256 rsqrt = _mm256_rsqrt_ps(rx2_ry2);
-//      __m256 fxd = _mm256_mul_ps(rsqrt,_mm256_mul_ps(rsqrt, _mm256_mul_ps(rsqrt, _mm256_mul_ps(mvec,rx))));
-//      __m256 fyd = _mm256_mul_ps(rsqrt,_mm256_mul_ps(rsqrt, _mm256_mul_ps(rsqrt, _mm256_mul_ps(mvec,ry))));
-      __m256 fxd = _mm256_div_ps(_mm256_div_ps(_mm256_div_ps(_mm256_mul_ps(rx,mvec),r),r),r);
-      __m256 fyd = _mm256_div_ps(_mm256_div_ps(_mm256_div_ps(_mm256_mul_ps(ry,mvec),r),r),r);
+//      __m256 r = _mm256_sqrt_ps(rx2_ry2);
+      __m256 rsqrt = _mm256_rsqrt_ps(rx2_ry2);
+      __m256 fxd = _mm256_mul_ps(rsqrt,_mm256_mul_ps(rsqrt, _mm256_mul_ps(rsqrt, _mm256_mul_ps(mvec,rx))));
+      __m256 fyd = _mm256_mul_ps(rsqrt,_mm256_mul_ps(rsqrt, _mm256_mul_ps(rsqrt, _mm256_mul_ps(mvec,ry))));
+//      __m256 fxd = _mm256_div_ps(_mm256_div_ps(_mm256_div_ps(_mm256_mul_ps(rx,mvec),r),r),r);
+//      __m256 fyd = _mm256_div_ps(_mm256_div_ps(_mm256_div_ps(_mm256_mul_ps(ry,mvec),r),r),r);
 	fxvec = _mm256_sub_ps(fxvec,fxd);
         fyvec = _mm256_sub_ps(fyvec,fyd);
   }
