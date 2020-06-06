213aba9ec59f8ffddcde15902564cf3715d1b699
19M38372 <peemapot57@gmail.com>
2020-06-06 21:02:35 +0900

    Updated

---
+++ 04_simd/14_test.cpp
@@ -0,0 +1,25 @@
+#include<cstdio>
+#include<immintrin.h>
+
+int main(){
+    const int N = 8;
+    float a[N], b[N], c[N];
+    for(int i=0; i<N; i++){
+       a[i] = i;
+       b[i] = i*0.1;
+       c[i] = 0;
+    }
+
+
+    __m256 avec = _mm256_load_ps(a);
+    __m256 bvec = _mm256_load_ps(b);
+    __m256 cvec = _mm256_mul_ps(avec, bvec);
+    _mm256_store_ps(a, avec);
+    _mm256_store_ps(b, bvec);
+    _mm256_store_ps(c, cvec);
+    for(int i=0; i<N; i++)
+	printf("%d: %g %g %g\n",i,a[i],b[i], c[i]);
+
+}
+
+
