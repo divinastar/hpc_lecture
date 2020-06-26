e3792b437562e9be839b09d800e47fb2768771ae
divinastar <56137998+divinastar@users.noreply.github.com>
2020-06-26 18:30:38 +0900

    Update Final_Report.cpp

--- 13_fdm/Final_Report.cpp
+++ 13_fdm/Final_Report.cpp
@@ -1,8 +1,31 @@
-#include<cstdio>
+#include <cstdio>
+
+void build_up_b(){
+   
+}
+
+void pressure_poisson_periodic(){
+   
+}
 
 int main() {
-   float u
-   float v
-   float p
+   //Variable Declarations
+   int nx = 41;
+   int ny = 41;
+   int nt = 10;
+   int nit = 50;
+   int c = 1;
+   float dx = 2/(nx - 1);
+   float dy = 2/(ny - 1);
    
+   
+   //Physical Variables
+   int rho = 1;
+   float nu = .1;
+   int F = 1;
+   float dt = .01;
+   
+   //Initial Conditions
+   
+      
 }
