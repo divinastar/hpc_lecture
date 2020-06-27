9d6067f323e0f88d9b1f884f7b0652d28feb4e96
divinastar <56137998+divinastar@users.noreply.github.com>
2020-06-27 20:41:13 +0900

    Update Final_Report.cpp

--- 13_fdm/Final_Report.cpp
+++ 13_fdm/Final_Report.cpp
@@ -8,8 +8,66 @@ const int nt = 10;
 const int nit = 50;
 const int c = 1;
 
+float build_up_b(int rho, float dt, float dx, float dy, float **u, float **v){
+   float b[ny][nx];
+   for(int i=0;i<nx;i++){
+      for(int j=0;j<ny;j++){
+         b[j][i] = 0;
+      }
+   }
+   
+   for(int i=1;i<nx-1;i++){
+      for(int j=1;j<ny-1;j++){
+         b[j][i] = (rho*(1/dt* ((u[j][i+1] - u[j][i-1])/(2*dx) 
+                              + (v[j+1][i] - v[j-1][i])/(2*dy))
+                              - pow((u[j][i+1] - u[j][i-1])/(2*dx),2)
+                              - 2*((u[j+1][i] - u[j-1][i])/(2*dy) *
+                                    (v[j][i+1]-v[j][i-1])/(2*dx)) -
+                                    pow((v[j+1][i] - v[j-1][i])/(2*dy),2)));
+      
+      }
+   }
+   //Periodic BC Pressure @x = 2
+   for(int j=1;j<ny;j++){
+      b[j][nx-1] = (rho*(1/dt*((u[j][0] - u[j][nx-2])/(2*dx)
+                              +(v[j+1][nx-1] - v[j-1][nx-1])/(2*dy))
+                              -pow((u[j][0] - u[j][nx-2])/(2*dx),2)
+                               - 2*((u[j+1][nx-1]-u[j-1][nx-1])/(2*dy) *
+                                    (v[j][0] - v[j][nx-2])/(2*dx)) -
+                                    pow((v[j+1][nx-1] - v[j-1][nx-1])/(2*dy),2)));
+   }
+                                   
+                                   
+   //Periodic BC Pressure @x = 0                               
+    for(int j=1;j<ny;j++){
+      b[j][0] = (rho*(1/dt*((u[j][1] - u[j][nx-1])/(2*dx)
+                              +(v[j+1][0] - v[j-1][0])   /(2*dy))
+                              - pow((u[j][1] - u[j][nx-1])/(2*dx),2)
+                               - 2*((u[j+1][0]-u[j-1][0])/(2*dy) *
+                                    (v[j][1] - v[j][nx-1])/(2*dx)) -
+                                    pow((v[j+1][0] - v[j-1][0])/(2*dy),2)));
+   }                                  
+   return b;
+}
 
-
+float [41][41] pressure_poisson_periodic(float **p, float dx, float dy){
+   float pn[ny][nx];
+   
+   for(int q=0; q<nit; q++){
+      for(int i=0;i<=nx;i++){
+         for(int j=0;j<=ny;j++){
+             pn[j][i] = p[j][i];
+         }
+      }
+      
+      //Periodic BC Pressure @ x = 2
+      //Periodic BC Pressure @ x = 0
+      //Wall boundary conditions, pressure
+       //dp/dy = 0 at y = 2
+       //dp/dy = 0 at y = 0
+   }
+   return p;
+}
 
 int main() {
    //Variable Declarations
@@ -91,65 +149,4 @@ int main() {
       udiff = (sumu - sumun)/ sumu ;
       stepcount += 1;     
    }
-}
-
-float [41][41] build_up_b(int rho, float dt, float dx, float dy, float **u, float **v){
-   float b[ny][nx];
-   for(int i=0;i<nx;i++){
-      for(int j=0;j<ny;j++){
-         b[j][i] = 0;
-      }
-   }
-   
-   for(int i=1;i<nx-1;i++){
-      for(int j=1;j<ny-1;j++){
-         b[j][i] = (rho*(1/dt* ((u[j][i+1] - u[j][i-1])/(2*dx) 
-                              + (v[j+1][i] - v[j-1][i])/(2*dy))
-                              - pow((u[j][i+1] - u[j][i-1])/(2*dx),2)
-                              - 2*((u[j+1][i] - u[j-1][i])/(2*dy) *
-                                    (v[j][i+1]-v[j][i-1])/(2*dx)) -
-                                    pow((v[j+1][i] - v[j-1][i])/(2*dy),2)));
-      
-      }
-   }
-   //Periodic BC Pressure @x = 2
-   for(int j=1;j<ny;j++){
-      b[j][nx-1] = (rho*(1/dt*((u[j][0] - u[j][nx-2])/(2*dx)
-                              +(v[j+1][nx-1] - v[j-1][nx-1])/(2*dy))
-                              -pow((u[j][0] - u[j][nx-2])/(2*dx),2)
-                               - 2*((u[j+1][nx-1]-u[j-1][nx-1])/(2*dy) *
-                                    (v[j][0] - v[j][nx-2])/(2*dx)) -
-                                    pow((v[j+1][nx-1] - v[j-1][nx-1])/(2*dy),2)));
-   }
-                                   
-                                   
-   //Periodic BC Pressure @x = 0                               
-    for(int j=1;j<ny;j++){
-      b[j][0] = (rho*(1/dt*((u[j][1] - u[j][nx-1])/(2*dx)
-                              +(v[j+1][0] - v[j-1][0])   /(2*dy))
-                              - pow((u[j][1] - u[j][nx-1])/(2*dx),2)
-                               - 2*((u[j+1][0]-u[j-1][0])/(2*dy) *
-                                    (v[j][1] - v[j][nx-1])/(2*dx)) -
-                                    pow((v[j+1][0] - v[j-1][0])/(2*dy),2)));
-   }                                  
-   return b;
-}
-
-float [41][41] pressure_poisson_periodic(float **p, float dx, float dy){
-   float pn[ny][nx];
-   
-   for(int q=0; q<nit; q++){
-      for(int i=0;i<=nx;i++){
-         for(int j=0;j<=ny;j++){
-             pn[j][i] = p[j][i];
-         }
-      }
-      
-      //Periodic BC Pressure @ x = 2
-      //Periodic BC Pressure @ x = 0
-      //Wall boundary conditions, pressure
-       //dp/dy = 0 at y = 2
-       //dp/dy = 0 at y = 0
-   }
-   return p;
 }
