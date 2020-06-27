1862a6cfd66f13b47e02cd96db06cb2336a4b76b
divinastar <56137998+divinastar@users.noreply.github.com>
2020-06-27 20:14:04 +0900

    Update Final_Report.cpp

--- 13_fdm/Final_Report.cpp
+++ 13_fdm/Final_Report.cpp
@@ -8,7 +8,7 @@ const int nt = 10;
 const int nit = 50;
 const int c = 1;
 
-float build_up_b(int rho, float dt, float dx, float dy, float u[], float v[]){
+float build_up_b(int rho, float dt, float dx, float dy, float **u, float **v){
    float b[ny][nx];
    for(int i=0;i<nx;i++){
       for(int j=0;j<ny;j++){
