094c96db801954ce204fc9b9ba30bc0e1b5955ab
divinastar <56137998+divinastar@users.noreply.github.com>
2020-06-27 20:25:03 +0900

    Update Final_Report.cpp

--- 13_fdm/Final_Report.cpp
+++ 13_fdm/Final_Report.cpp
@@ -8,7 +8,7 @@ const int nt = 10;
 const int nit = 50;
 const int c = 1;
 
-float** build_up_b(int rho, float dt, float dx, float dy, float **u, float **v){
+float * build_up_b(int rho, float dt, float dx, float dy, float **u, float **v){
    float b[ny][nx];
    for(int i=0;i<nx;i++){
       for(int j=0;j<ny;j++){
@@ -50,7 +50,7 @@ float** build_up_b(int rho, float dt, float dx, float dy, float **u, float **v){
    return b;
 }
 
-float** pressure_poisson_periodic(float p, float dx, float dy){
+float * pressure_poisson_periodic(float p, float dx, float dy){
    float pn[ny][nx];
    
    for(int q=0; q<nit; q++){
