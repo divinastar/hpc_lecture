8ab3e2dfa2d0f728cd8f2287eb418ea6144cc2e4
Peemapot Uparakool <uparakool.p.aa@adminnoMacBook-Pro.local>
2020-06-29 01:14:11 +0900

    ifnal

--- 13_fdm/Final_Report_CUDA.cu
+++ 13_fdm/Final_Report_CUDA.cu
@@ -254,15 +254,15 @@ int main() {
       }
       
       build_up_b<<<ny,nx>>>(b,rho,dt,dx,dy,u,v);
-      std::cout<<cudaGetLastError()<<std::endl;
+      std::cout<<cudaGetErrorString(cudaGetLastError())<<std::endl;
       cudaDeviceSynchronize();
       //b = build_up_b(rho, dt, dx, dy, u, v);
       pressure_poisson_periodic<<<ny,nx>>>(p,pn.data(),b, dx, dy);
-      std::cout<<cudaGetLastError()<<std::endl;
+      std::cout<<cudaGetErrorString(cudaGetLastError())<<std::endl;
       cudaDeviceSynchronize();
       //p = pressure_poisson_periodic(p, b, dx, dy);
       updated_u_v<<<ny,nx>>>();//u,v);//,un.data(),vn.data(),p,dx,dy,dt,rho,nu,F);
-      std::cout<<cudaGetLastError()<<std::endl;
+      std::cout<<cudaGetErrorString(cudaGetLastError())<<std::endl;
       cudaDeviceSynchronize();
 
       for(int i=0;i<nx;i++){
