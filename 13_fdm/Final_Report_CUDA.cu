4101fbad45d0269a040b9ae3fb8401578471f1a0
Peemapot Uparakool <uparakool.p.aa@adminnoMacBook-Pro.local>
2020-06-29 09:53:38 +0900

    Final

--- 13_fdm/Final_Report_CUDA.cu
+++ 13_fdm/Final_Report_CUDA.cu
@@ -7,9 +7,9 @@ using namespace std;
 
 const int nx = 41;
 const int ny = 41;
-const int nt = 10;
+//const int nt = 10;
 const int nit = 50;
-const int c = 1;
+//const int c = 1;
 
 __global__ void build_up_b(float *b, int rho, float dt, float dx, float dy, float *u , float *v){
    //m = j * nx + i
